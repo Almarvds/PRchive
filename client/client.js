9b1905d2546e605394c646850580dc8e8b9648cc
Almarvds <a.r.v.d.stappen@student.tue.nl>
2020-06-27 20:15:27 +0200

    interface hidden on submit

--- client/client.js
+++ client/client.js
@@ -1,1 +1,17 @@
 console.log('client.js called')
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form)
+  const username = formData.get('username')
+  const password = formData.get('password')
+
+  const loginData = {
+    username,
+    password
+  }
+  form.style.display = 'none';
+  console.log(loginData)
+})
